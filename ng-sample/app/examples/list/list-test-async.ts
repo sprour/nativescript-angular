ee2e4abbd275fa35f8f7bc29539ad809d9a30a35
vakrilov <alexander.vakrilov@telerik.com>
2016-06-08 17:37:09 +0300

    Updated async examples

--- ng-sample/app/examples/list/list-test-async.ts
+++ ng-sample/app/examples/list/list-test-async.ts
@@ -1,58 +1,95 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
-import { Observable as RxObservable } from 'rxjs/Observable';
-
-export class DataItem {
-    constructor(public id: number, public name: string) { }
-}
+import * as Rx from 'rxjs/Observable';
+import { combineLatestStatic } from 'rxjs/operator/combineLatest';
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { DataItem, DataService } from "./data.service"
 
 @Component({
     selector: 'list-test-async',
     styleUrls: ['examples/list/list-test-async.css'],
+    providers: [DataService],
     template: `
-    <GridLayout>
-        <ListView [items]="myItems | async" (itemTap)="onItemTap($event)">
+    <GridLayout rows="* auto">
+        <ListView [items]="service.items$ | async" (itemTap)="onItemTap($event)">
             <template let-item="item" let-i="index" let-odd="odd" let-even="even">
                 <StackLayout [class.odd]="odd" [class.even]="even">
                     <Label class="test" [text]='"index: " + item.name'></Label>
                 </StackLayout>
             </template>
         </ListView>
+        <button row="1" (tap)="toggleAsyncUpdates()" [text]="isUpdating ? 'stop updates' : 'start updates'"></button>
     </GridLayout>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListTestAsync {
-    public myItems: RxObservable<Array<DataItem>>;
-
-    constructor() {
-        var items = [];
-        for (var i = 0; i < 3; i++) {
-            items.push(new DataItem(i, "data item " + i));
-        }
-        
-        var subscr;
-        this.myItems = RxObservable.create(subscriber => {
-            subscr = subscriber;
-            subscriber.next(items);
-            return function () {
-                console.log("Unsubscribe called!!!");
-            }
-        });
-
-        let counter = 2;
-        let intervalId = setInterval(() => {
-            counter++;
-            console.log("Adding " + counter + "-th item");
-            items.push(new DataItem(counter, "data item " + counter));
-            subscr.next(items);
-        }, 1000);
-        
-        setTimeout(() => {
-            clearInterval(intervalId);   
-        }, 15000);
+    public isUpdating: boolean = false;
+    constructor(private service: DataService) {
     }
 
     public onItemTap(args) {
-        console.log("------------------------ ItemTapped: " + args.index);
+        console.log("--> ItemTapped: " + args.index);
+    }
+
+    public toggleAsyncUpdates() {
+        if (this.isUpdating) {
+            this.service.stopAsyncUpdates();
+        } else {
+            this.service.startAsyncUpdates();
+        }
+
+        this.isUpdating = !this.isUpdating;
+    }
+}
+
+@Component({
+    selector: 'list-test-async-filter',
+    styleUrls: ['examples/list/list-test-async.css'],
+    providers: [DataService],
+    template: `
+    <GridLayout rows="* auto">
+        <ListView [items]="filteredItems$ | async" (itemTap)="onItemTap($event)">
+            <template let-item="item" let-i="index" let-odd="odd" let-even="even">
+                <StackLayout [class.odd]="odd" [class.even]="even">
+                    <Label class="test" [text]='"index: " + item.name'></Label>
+                </StackLayout>
+            </template>
+        </ListView>
+        <StackLayout row="1" orientation="horizontal">
+            <button (tap)="toggleAsyncUpdates()" [text]="isUpdating ? 'stop updates' : 'start updates'"></button>
+            <button (tap)="toogleFilter()" [text]="(filter$ | async) ? 'show all' : 'show even'"></button>
+        </StackLayout>
+    </GridLayout>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ListTestFilterAsync {
+    public isUpdating: boolean = false;
+    public filteredItems$: Rx.Observable<Array<DataItem>>;
+    private filter$ = new BehaviorSubject(false);
+
+    constructor(private service: DataService) {
+        // Create filteredItems$ by combining the service.items$ and filter$
+        this.filteredItems$ = combineLatestStatic(this.service.items$, this.filter$, (data, filter) => {
+            return filter ? data.filter(v => v.id % 2 === 0) : data;
+        });
+    }
+
+    public onItemTap(args) {
+        console.log("--> ItemTapped: " + args.index);
+    }
+
+    public toggleAsyncUpdates() {
+        if (this.isUpdating) {
+            this.service.stopAsyncUpdates();
+        } else {
+            this.service.startAsyncUpdates();
+        }
+
+        this.isUpdating = !this.isUpdating;
+    }
+
+    public toogleFilter() {
+        this.filter$.next(!this.filter$.value);
     }
 }
